b4c100dda726a777ec2e778364901b5cb441102c
Francoviera <vierafranco1@gmail.com>
2021-10-01 14:39:18 -0300

    se implemento el main

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,10 @@ package main;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import DTO.ReporteCarreras;
 import registro.estudiantes.CSVtoMYSQL;
+import registro.estudiantes.dao.Estudiante;
 import repository.implementation.CarreraImplementation;
 import repository.implementation.CiudadImplementation;
 import repository.implementation.EstudianteImplementation;
@@ -25,6 +28,8 @@ public class Main {
 		FacultadImplementation facultadImplementation = new FacultadImplementation(emf.createEntityManager());
 		SituacionAcademicaImplementation situacionAcademicaImplementation = new SituacionAcademicaImplementation(
 				emf.createEntityManager());
+		
+		ReporteCarreras reporteCarreras= new ReporteCarreras();
 
 		/**
 		 * Importar Datos del CSV
@@ -41,6 +46,55 @@ public class Main {
 		/**
 		 * Operaciones
 		 */
+		
+		/**
+		 * 2) A
+		 * dar de alta a un estudiante
+		 */
+			Estudiante estudiante= new Estudiante("benjamin", "hoffman", (long) 4333333, "difuso");
+		
+			System.out.println(estudianteImplementation.saveEstudiante(estudiante));
+			
+		/**
+		 * 2) B
+		 * matricular un estudiante a una carrera
+		 */
+			facultadImplementation.matricularEstudiante(4333333, "African black crake");
+		
+		/**
+		 * 2) C
+		 * recupera todos los estudiante con un ordenamiento por apellido
+		 */
+			System.out.println(estudianteImplementation.getEstudiantesSortByApellido());
+		
+		/**
+		 * 2) D
+		 * recupera un estudiante en base de su libreta universitaria
+		 */
+			System.out.println(estudianteImplementation.getEstudianteByID(2));
+		/**
+		 * 2) E
+		 * recupera todos los estudiante en base a su genero
+		 */
+			System.out.println(estudianteImplementation.getEstudiantesByGenero("Male"));
+		/**
+		 * 2) F
+		 * recupera las carreras con estudiantes inscriptos y ordenar por cantidad de inscriptos
+		 */
+			System.out.println(carreraImplementation.getCarrerasConEstudiantesSortByCantidad());
+		/**
+		 * 2) G
+		 * recupera los estudiantes por una determinada carrera, filtrados por ciudad de residencia
+		 */
+			System.out.println(estudianteImplementation.getEstudiantesByCiudad(1, 2));
+		/**
+		 * 3
+		 * Generar un reporte de las carreras, que para cada carrera incluya información de los
+		 *inscriptos y egresados por año. Se deben ordenar las carreras alfabéticamente, y presentar
+		 *los años de manera cronológica.
+		 */
+			System.out.println(reporteCarreras.toString());
+			
 
 		/**
 		 * Cierre de conexiones
